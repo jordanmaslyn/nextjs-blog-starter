324ed1f03a8561293659ba88eaf66208faab1c92
Jordan Maslyn <jordan.maslyn@webflow.com>
2025-08-19 11:02:21 -0400

    Remove assetPrefix config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ module.exports = {
     // Configure the base path and asset prefix to reflect the mount path of your environment
     // For example, if your app is mounted at /app, set basePath and assetPrefix to '/app'
     basePath: '/nextjs-blog',
-    assetPrefix: '/nextjs-blog',
+    // assetPrefix: '/nextjs-blog',
     
     // Additional Next.js configuration options can be added here
     // For example:
